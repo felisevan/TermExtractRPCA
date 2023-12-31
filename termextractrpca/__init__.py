95ccb2077530a671ab6f38b8d2424211fbc6a459
14MBD4 <Salvationliewithin@protonmail.com>
2023-12-31 05:58:31 -0400

    add functional methods

--- termextractrpca/__init__.py
+++ termextractrpca/__init__.py
@@ -1,8 +1,11 @@
-import stopwordsiso as stopwords
-import sklearn.feature_extraction
-import robust_pca
+from collections import Counter
+
 import nltk
+import sklearn.feature_extraction
+import stopwordsiso as stopwords
 import torch
+
+import robust_pca
 
 
 def get_keywords(s):
@@ -24,6 +27,28 @@ def get_keywords(s):
         key=lambda x: x[1],
         reverse=True,
     )
+
+
+def into_token(inputfile, output='corpus_output.txt'):
+    """处理原始文档：去除标点符号和纯数字，以及空行"""
+    with open(inputfile, 'r', encoding='utf-8') as fp:
+        lines = fp.readlines()
+        text = ''.join(re.sub(r'(?<=\b)\d+(?=\b)|[^\w\s]', '', line).lower() for line in lines if line != '\n'
+                       )  # punctuation marks and numbers between two space  and drop vacuum lines
+    with open(output, 'w', encoding='utf-8') as fp:
+        fp.write(text)
+
+
+def vocab_count(file):
+    """全文词表  {单词：计数}"""
+    vocabulary = list()
+    with open(file, 'r', encoding='utf-8') as fp:
+        text = fp.read()
+        words = text.split()
+        vocabulary.extend(words)
+        # print(vocabulary)
+    words_count = Counter(vocabulary)
+    return words_count
 
 
 if __name__ == "__main__":
